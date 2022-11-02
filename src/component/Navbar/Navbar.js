8b10897f41216f928cd7f9e63bd1ea0ca1a21d0b
florian maugan <florian.maugan@gmail.com>
2022-11-02 10:34:28 +0100

    add navbar

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+const Navbar = () => {
+
+    return (
+        <div className='navbar'>
+            <ul>
+                <li>Compétences</li>
+                <li>Skills</li>
+                <li>Expériences professionnelles</li>
+                <li>Formations</li>
+                <li>Langues</li>
+                <li>Qualités</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
